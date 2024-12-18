3d17a33da06abdbb7317254a63c1a2509a10aaed
poorphd <bc.park@bharvest.io>
2024-12-18 18:40:37 +0900

    module parameters type definition and methods

--- x/amm/types/params.go
+++ x/amm/types/params.go
@@ -1,22 +1,73 @@
 package types
 
 import (
+	"fmt"
+
+	"cosmossdk.io/math"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
 )
 
 var _ paramstypes.ParamSet = (*Params)(nil)
 
-func NewParams() Params {
-	return Params{}
+var (
+	KeyFeeRate             = []byte("FeeRate")
+	KeyMinInitialLiquidity = []byte("MinInitialLiquidity")
+
+	DefaultFeeRate             = math.LegacyNewDecWithPrec(3, 3)
+	DefaultMinInitialLiquidity = math.NewInt(1000)
+)
+
+// NewParams creates a new Params instance
+func NewParams(feeRate math.LegacyDec, minInitialLiquidity math.Int) Params {
+	return Params{
+		FeeRate:             feeRate,
+		MinInitialLiquidity: minInitialLiquidity,
+	}
 }
 
+// DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(
+		DefaultFeeRate,
+		DefaultMinInitialLiquidity,
+	)
 }
+
 func (params *Params) Validate() error {
+	if err := validateFeeRate(params.FeeRate); err != nil {
+		return err
+	}
+	if err := validateMinInitialLiquidity(params.MinInitialLiquidity); err != nil {
+		return err
+	}
 	return nil
 }
+
+func validateFeeRate(v interface{}) error {
+	feeRate, ok := v.(math.LegacyDec)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+	if feeRate.IsNegative() {
+		return fmt.Errorf("invalid parameter value: %v", feeRate)
+	}
+
+	return nil
+}
+
+func validateMinInitialLiquidity(v interface{}) error {
+	minInitialLiquidity, ok := v.(math.Int)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+	if minInitialLiquidity.IsNegative() {
+		return fmt.Errorf("invalid parameter value: %v", minInitialLiquidity)
+	}
+
+	return nil
+}
+
 func (params *Params) String() string {
 	out, _ := yaml.Marshal(params)
 	return string(out)
@@ -28,5 +79,8 @@ func ParamKeyTable() paramstypes.KeyTable {
 }
 
 func (params *Params) ParamSetPairs() paramstypes.ParamSetPairs {
-	return paramstypes.ParamSetPairs{}
+	return paramstypes.ParamSetPairs{
+		paramstypes.NewParamSetPair(KeyFeeRate, &params.FeeRate, validateFeeRate),
+		paramstypes.NewParamSetPair(KeyMinInitialLiquidity, &params.MinInitialLiquidity, validateMinInitialLiquidity),
+	}
 }
