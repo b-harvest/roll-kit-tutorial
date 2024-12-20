1a48acf7f6bec094de6cdb91a05027fbb771e71b
poorphd <bc.park@bharvest.io>
2024-12-20 19:41:17 +0900

    cli implementation for querying pair information

--- x/amm/autocli.go
+++ x/amm/autocli.go
@@ -1,7 +1,10 @@
 package amm
 
 import (
+	"fmt"
+
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+	"github.com/cosmos/cosmos-sdk/version"
 
 	ammapi "github.com/b-harvest/roll-kit-tutorial/api/simapp/amm/v1beta1"
 )
@@ -15,6 +18,20 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					RpcMethod: "Params",
 					Use:       "params",
 					Short:     "Query the current coinswap module parameters information",
+				},
+				{
+					RpcMethod: "Pairs",
+					Use:       "pairs",
+					Short:     "Query all pairs",
+				},
+				{
+					RpcMethod: "Pair",
+					Use:       "pair <pair_id>",
+					Short:     "Query a pair",
+					Example:   fmt.Sprintf("$ %s query amm pair 1", version.AppName),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "id"},
+					},
 				},
 			},
 		},
